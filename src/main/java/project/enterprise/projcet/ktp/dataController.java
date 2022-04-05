ff329855589499fa4f8494fbf97580ef7ec9ed33
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-04-05 15:22:21 +0700

    pertemuan 7

--- src/main/java/project/enterprise/projcet/ktp/dataController.java
+++ src/main/java/project/enterprise/projcet/ktp/dataController.java
@@ -4,17 +4,33 @@
  */
 package project.enterprise.projcet.ktp;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author hp
- */
 @Controller
 public class dataController {
+    
+    DataJpaController ctrl = new DataJpaController();
+    List<Data> newData = new ArrayList<>();
+    
     @RequestMapping("/data")
-    public String dataKTP(){
+    //@ResponseBody
+    public String getDataKTP(Model model) {
+        int record = ctrl.getDataCount();
+        String result ="";
+        try {
+            newData = ctrl.findDataEntities().subList(0, record);
+        } catch (Exception e) {
+            result=e.getMessage();
+        }
+        model.addAttribute("newData", newData);
+        model.addAttribute("record", record);
+        
+        
         return "database";
     }
+    
 }
