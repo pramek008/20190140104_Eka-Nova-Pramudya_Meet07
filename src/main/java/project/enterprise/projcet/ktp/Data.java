bc3aff0cdc91d37a2af76ad3f6b2b847c4701181
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-04-05 14:58:38 +0700

    pertemuan 7

---
+++ src/main/java/project/enterprise/projcet/ktp/Data.java
@@ -0,0 +1,226 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.enterprise.projcet.ktp;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author hp
+ */
+@Entity
+@Table(name = "data")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Data.findAll", query = "SELECT d FROM Data d"),
+    @NamedQuery(name = "Data.findById", query = "SELECT d FROM Data d WHERE d.id = :id"),
+    @NamedQuery(name = "Data.findByNoKtp", query = "SELECT d FROM Data d WHERE d.noKtp = :noKtp"),
+    @NamedQuery(name = "Data.findByNama", query = "SELECT d FROM Data d WHERE d.nama = :nama"),
+    @NamedQuery(name = "Data.findByTglLahir", query = "SELECT d FROM Data d WHERE d.tglLahir = :tglLahir"),
+    @NamedQuery(name = "Data.findByJenisKelamin", query = "SELECT d FROM Data d WHERE d.jenisKelamin = :jenisKelamin"),
+    @NamedQuery(name = "Data.findByAlmat", query = "SELECT d FROM Data d WHERE d.almat = :almat"),
+    @NamedQuery(name = "Data.findByAgama", query = "SELECT d FROM Data d WHERE d.agama = :agama"),
+    @NamedQuery(name = "Data.findByStatus", query = "SELECT d FROM Data d WHERE d.status = :status"),
+    @NamedQuery(name = "Data.findByPekerjaan", query = "SELECT d FROM Data d WHERE d.pekerjaan = :pekerjaan"),
+    @NamedQuery(name = "Data.findByWarganegara", query = "SELECT d FROM Data d WHERE d.warganegara = :warganegara"),
+    @NamedQuery(name = "Data.findByMasaBerlaku", query = "SELECT d FROM Data d WHERE d.masaBerlaku = :masaBerlaku")})
+public class Data implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "no_ktp")
+    private String noKtp;
+    @Basic(optional = false)
+    @Column(name = "nama")
+    private String nama;
+    @Basic(optional = false)
+    @Column(name = "tgl_lahir")
+    @Temporal(TemporalType.DATE)
+    private Date tglLahir;
+    @Basic(optional = false)
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+    @Basic(optional = false)
+    @Column(name = "almat")
+    private String almat;
+    @Basic(optional = false)
+    @Column(name = "agama")
+    private String agama;
+    @Basic(optional = false)
+    @Column(name = "status")
+    private String status;
+    @Column(name = "pekerjaan")
+    private String pekerjaan;
+    @Basic(optional = false)
+    @Column(name = "warganegara")
+    private String warganegara;
+    @Basic(optional = false)
+    @Column(name = "masa_berlaku")
+    private String masaBerlaku;
+    @Lob
+    @Column(name = "foto")
+    private byte[] foto;
+
+    public Data() {
+    }
+
+    public Data(Long id) {
+        this.id = id;
+    }
+
+    public Data(Long id, String noKtp, String nama, Date tglLahir, String jenisKelamin, String almat, String agama, String status, String warganegara, String masaBerlaku) {
+        this.id = id;
+        this.noKtp = noKtp;
+        this.nama = nama;
+        this.tglLahir = tglLahir;
+        this.jenisKelamin = jenisKelamin;
+        this.almat = almat;
+        this.agama = agama;
+        this.status = status;
+        this.warganegara = warganegara;
+        this.masaBerlaku = masaBerlaku;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Date getTglLahir() {
+        return tglLahir;
+    }
+
+    public void setTglLahir(Date tglLahir) {
+        this.tglLahir = tglLahir;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAlmat() {
+        return almat;
+    }
+
+    public void setAlmat(String almat) {
+        this.almat = almat;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public String getWarganegara() {
+        return warganegara;
+    }
+
+    public void setWarganegara(String warganegara) {
+        this.warganegara = warganegara;
+    }
+
+    public String getMasaBerlaku() {
+        return masaBerlaku;
+    }
+
+    public void setMasaBerlaku(String masaBerlaku) {
+        this.masaBerlaku = masaBerlaku;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Data)) {
+            return false;
+        }
+        Data other = (Data) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "project.enterprise.projcet.ktp.Data[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/project/enterprise/projcet/ktp/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package project.enterprise.projcet.ktp.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
